e6fa3da2d0bc09b08ad1ff7fd1798f9234aae96c
jorge-alonso <jorge.a.alonso91@gmail.com>
2022-04-29 16:54:21 -0300

    práctica completa

---
+++ app/src/main/java/com/ort/tabsappjorge/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ort.tabsappjorge.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ort.tabsappjorge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
